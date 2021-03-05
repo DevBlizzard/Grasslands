df0356b62d0016ed5a9888879706f1003ba2b8d2
DevBlizzard <79875961+DevBlizzard@users.noreply.github.com>
2021-03-05 15:06:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var lastCalledTime;
-var fps;
-
-function requestAnimFrame() {
-
-  if(!lastCalledTime) {
-     lastCalledTime = Date.now();
-     fps = 0;
-     return;
-  }
-  delta = (Date.now() - lastCalledTime)/1000;
-  lastCalledTime = Date.now();
-  fps = 1/delta;
-}
-requestAnimFrame()
 class Block {
   constructor(x, y, z) {
     this.x = x;
